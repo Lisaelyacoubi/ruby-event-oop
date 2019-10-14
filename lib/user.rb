33380f219ef8028ecb918a709773b5e81a787ac4
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-10-14 11:51:35 +0200

    class User

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,21 @@
-class User
+require 'pry'
 
+class User #création de la classe User
+	attr_accessor :email, :age #accès aux variables d'instance
+	@@user_all = 0
+
+	def initialize(email_to_save, age_to_update)
+		@email = email_to_save
+		@age = age_to_update.to_i #integer
+		@@user_all = 1 + @@user_all
+
+	end
+
+	def self.all
+		return [@@user_all]
+	end
 end
+
+
+binding.pry
+puts "end of file"
