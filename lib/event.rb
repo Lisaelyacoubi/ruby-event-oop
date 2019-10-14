32d9dc83f664b7dc75caa844de27478a05d60e97
Lisaelyacoubi <lisa.elyacoubi@gmail.com>
2019-10-14 17:42:47 +0200

    exo 2.3.3. c

--- lib/event.rb
+++ lib/event.rb
@@ -8,6 +8,14 @@ Récupérer infos : date de fin, event passé ou event dans 30 min
 Afficher l'event
 =end
 
+=begin
+    Connaître la date de fin ;
+    Savoir si un événement a déjà eu lieu (est-ce qu'il a commencé / est fini) ;
+    Savoir si au contraire un événement est dans le futur ;
+    Savoir si un événement est bientôt (dans moins de 30 minutes) ;
+=end 
+	
+
 class Event
 	attr_accessor :start_date, :duration, :title, :attendees #accès aux variables d'instance pour modification
 
@@ -20,6 +28,33 @@ class Event
 		def self.postpone_24h #Décale l'évènement au lendemain
 			return @start_date = @start_date + 24*60*60
 		end
+
+		def self.end_date
+			@end_date = @start_date + @duration 
+		end
+
+		def self.is_past?
+			#comparer start_date avec la date actuelle
+			if @start_date == Time.now
+				puts "Event today"
+			elsif @start_date < Time.now
+				puts "Event past"
+			else
+				puts "Event today"
+			end
+		end
+
+		def self.is_futur?
+			if @start_date > Time.now
+				puts "Event in the futur"
+			elsif
+				@start_date < Time.now
+				puts "Event past"
+			else
+				puts "Event today"
+			end
+		end
+				
 	end
 end
 
